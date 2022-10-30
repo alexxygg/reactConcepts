47a9b1793ecc8c16fdd7aa314ded77f9a0664e46
alexxygg <alexxygg@gmail.com>
2022-10-29 21:56:43 -0700

    Add files via upload

---
+++ hello-world/src/components/InlineCSS.js
@@ -0,0 +1,13 @@
+function InlineCSS() {
+  const styles = {
+    color: "blue",
+    fontSize: "xx-large",
+    backgroundColor: "green",
+    width: "50vw",
+    marginLeft: "25vw",
+  };
+
+  return <div style={styles}>InlineCSS</div>;
+}
+
+export default InlineCSS;
