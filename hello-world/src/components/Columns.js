6ac1494c00af330c6f43591c4e05d79143831265
alexxygg <alexxygg@gmail.com>
2022-11-01 21:04:35 -0700

    Add files via upload

---
+++ hello-world/src/components/Columns.js
@@ -0,0 +1,12 @@
+function Columns() {
+  return (
+    <>
+      <td>A column</td>
+      <td>A column</td>
+      <td>A column</td>
+      <td>A 4TH column</td>
+    </>
+  );
+}
+
+export default Columns;
